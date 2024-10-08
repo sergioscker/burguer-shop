0cae1f8c948afede1d0a9bc16e1879dad4bea7d2
sergioliveira-developer <sergiowallace11@hotmail.com>
2024-10-08 23:01:02 +0100

    Added Stripe API key

--- interface/src/config/stripeConfig.js
+++ interface/src/config/stripeConfig.js
@@ -1,7 +1,5 @@
 import { loadStripe } from '@stripe/stripe-js';
 
-const stripePromise = loadStripe(
-  'pk_test_51Q6y2QRqpypTkKXDAcpkbQiqP6FXFe0ce6usSRQE0emNQSYkzrevmgzesLqbD5QQCF0cCvFOs6LmoDj6yUATnCjM00O853Tx9i',
-);
+const stripePromise = loadStripe(process.env.STRIPE_API_KEY);
 
 export default stripePromise;
