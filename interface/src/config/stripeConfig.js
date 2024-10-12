cddc50b4c876a577662300277757f3c09cf707d9
sergioliveira-developer <sergiowallace11@hotmail.com>
2024-10-12 15:38:58 +0100

    passing Stripe's public key directly into the stripeConfig.js file for recognition on Railway.

--- interface/src/config/stripeConfig.js
+++ interface/src/config/stripeConfig.js
@@ -1,5 +1,9 @@
 import { loadStripe } from '@stripe/stripe-js';
 
-const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLIC_KEY);
+const stripePromise = loadStripe(
+  'pk_test_51Q6y2QRqpypTkKXDAcpkbQiqP6FXFe0ce6usSRQE0emNQSYkzrevmgzesLqbD5QQCF0cCvFOs6LmoDj6yUATnCjM00O853Tx9i',
+);
+
+console.log(stripePromise);
 
 export default stripePromise;
