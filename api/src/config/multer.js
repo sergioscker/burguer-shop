4c9e311ed113f1f2b28e8d0e4e111c5f7b62fc4c
sergioliveira-developer <sergiowallace11@hotmail.com>
2024-10-14 16:16:58 +0100

    Add new aws-sdk/client-s3.

--- api/src/config/multer.js
+++ api/src/config/multer.js
@@ -1,15 +1,17 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-import aws from 'aws-sdk';
-import multerS3 from 'multer-s3';
+import { S3Client } from '@aws-sdk/client-s3';
+import multerS3, { AUTO_CONTENT_TYPE } from 'multer-s3';
 import { extname } from 'path';
 import { v4 as uuidv4 } from 'uuid';
 
 // Credenciais da AWS
-const s3 = new aws.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID, // chave de acesso
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY, // chave secreta
+const s3 = new S3Client({
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID, // chave de acesso
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY, // chave secreta
+  },
   region: process.env.AWS_REGION, // Região do bucket
 });
 
@@ -18,7 +20,7 @@ export default {
   storage: multerS3({
     s3,
     bucket: process.env.AWS_S3_BUCKET_NAME, // Nome do bucket no S3
-    contentType: multerS3.AUTO_CONTENT_TYPE, // Para definir o tipo MIME automaticamente
+    contentType: AUTO_CONTENT_TYPE, // Para definir o tipo MIME automaticamente
     acl: 'public-read', // Definir permissões de leitura pública
     key: (_, file, callback) => {
       const fileName = `${uuidv4()}${extname(file.originalname)}`; // Nome único com uuid
